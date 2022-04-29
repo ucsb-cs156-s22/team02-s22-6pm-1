3d6a4998b3046502520d834b77c090a020340422
Timothy Chang <timothychang@ucsb.edu>
2022-04-28 23:02:06 -0700

    for testing purposes

--- src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-
 import java.time.LocalDateTime;
 
 @Api(description = "MenuItemReview")
